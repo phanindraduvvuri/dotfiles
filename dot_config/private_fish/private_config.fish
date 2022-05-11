c28054d182de6463f9adb951d9a9a9ad496ccefd
Phanindra Duvvuri <phanindra40430@gmail.com>
2022-05-11 09:59:33 +0530

    Update private_config.fish

--- dot_config/private_fish/private_config.fish
+++ dot_config/private_fish/private_config.fish
@@ -6,7 +6,7 @@ set fish_greeting  # Remove custom greeting
 ### "bat" as manpager
 set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
 ## Necessary for formatting problem when using bat
-set -x MANROFFOPT="-c" 
+export MANROFFOPT="-c"
 
 ### "vim" as manpager
 # set -x MANPAGER '/bin/bash -c "vim -MRn -c \"set buftype=nofile showtabline=0 ft=man ts=8 nomod nolist norelativenumber nonu noma\" -c \"normal L\" -c \"nmap q :qa<CR>\"</dev/tty <(col -b)"'
